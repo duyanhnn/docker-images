810ebf48b6698dc8fd64aa0838cc273fb8e84503
duyanhnn <duyanhnguyenngoc97@gmail.com>
2018-09-24 00:32:31 +0700

    msg.

--- setUpEnvAmazonUbuntu.sh
+++ setUpEnvAmazonUbuntu.sh
@@ -17,3 +17,5 @@ sudo apt-get install -y docker-ce
 sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
+sudo groupadd docker
+sudo gpasswd -a ubuntu docker
--- ssh/onlyu-ec2.sh
+++ ssh/onlyu-ec2.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 chmod 400 onlyuuuuu.pem
-ssh -i onlyuuuuu.pem ubuntu@ec2-54-175-83-207.compute-1.amazonaws.com
+ssh -i onlyuuuuu.pem ubuntu@ec2-34-204-42-253.compute-1.amazonaws.com
