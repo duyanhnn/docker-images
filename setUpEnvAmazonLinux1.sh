420a235639cb15e029d33064fbe6031bef927af7
duyanhnn <duyanhnguyenngoc97@gmail.com>
2018-09-23 22:49:15 +0700

    msg.

--- setUpEnvAmazonLinux1.sh
+++ setUpEnvAmazonLinux1.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-sudo yum install -y git
 sudo yum update -y
 sudo yum install -y docker
 sudo service docker start
 sudo usermod -a -G docker ec2-user
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
+sudo chmod +x /usr/local/bin/docker-compose
+sudo service docker start
+sudo chkconfig docker on
 docker info
