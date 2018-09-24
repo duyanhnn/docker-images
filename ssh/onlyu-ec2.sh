e0b4490c993ea66beceb6c2b106d4d0e2aac7021
duyanhnn <duyanhnguyenngoc97@gmail.com>
2018-09-24 16:02:38 +0700

    msg.

--- ssh/onlyu-ec2.sh
+++ ssh/onlyu-ec2.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 chmod 400 onlyuuuuu.pem
-ssh -i onlyuuuuu.pem ubuntu@ec2-18-207-226-121.compute-1.amazonaws.com
+ssh -i onlyuuuuu.pem ubuntu@ec2-184-73-66-77.compute-1.amazonaws.com
