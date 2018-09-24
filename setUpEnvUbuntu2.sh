6500588ba9c4cb03855318fdbd4aea6bf0c41366
duyanhnn <duyanhnguyenngoc97@gmail.com>
2018-09-24 14:54:31 +0700

    msg.

--- setUpEnvUbuntu2.sh
+++ setUpEnvUbuntu2.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
 echo 'Setting up swap memory.'
-sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=5012
-sudo /sbin/mkswap /var/swap.1
-sudo chmod 600 /var/swap.1
-sudo /sbin/swapon /var/swap.1
+sudo mount /dev/xvda2 /mnt
+sudo dd if=/dev/zero of=/mnt/swapfile bs=1M count=5120
+sudo chown root:root /mnt/swapfile
+sudo chmod 600 /mnt/swapfile
+sudo mkswap /mnt/swapfile
+sudo swapon /mnt/swapfile
+df -T
 sudo echo '' >> /etc/fstab
-sudo echo '/var/swap.1	swap	swap	defaults		0	0' >> /etc/fstab
+sudo echo '/dev/xvda1	/mnt	auto	defaults,nobootwait,comment=cloudconfig		0	2' >> /etc/fstab
+sudo echo '/mnt/swapfile	swap	swap	defaults		0	0' >> /etc/fstab
+sudo swapon -a
+free -m
 echo echo 'Done setting up swap memory.'
 sudo apt-get update -y
 sudo apt-get install -y \
