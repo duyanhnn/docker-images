c3e1026cfaa45cd1acdb01845f64a7ea68cd2f6a
duyanhnn <duyanhnguyenngoc97@gmail.com>
2018-10-05 15:54:54 +0700

    msg.

--- ssh/y4s1/y4s1-compute-engine.sh
+++ ssh/y4s1/y4s1-compute-engine.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+gcloud config set account `onlyu.year4sem1@gmail.com`
+gcloud config set project y4s1-tlcn
 gcloud compute --project "y4s1-tlcn" ssh --zone "asia-southeast1-b" "tlcn-instance-1"
