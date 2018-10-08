bc61200111841556f76da004ba93a07148abcdbc
duyanhnn <duyanhnguyenngoc97@gmail.com>
2018-10-08 11:03:29 +0700

    msg.

--- ssh/duyanhnguyenngoc16/onlyu-compute-engine2.sh
+++ ssh/duyanhnguyenngoc16/onlyu-compute-engine2.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-gcloud config set account `duyanhnguyenngoc16@gmail.com`
+gcloud config set account "duyanhnguyenngoc16@gmail.com"
 gcloud config set project onlyu-216914
 gcloud compute --project "onlyu-216914" ssh --zone "asia-southeast1-b" "instance-2"
