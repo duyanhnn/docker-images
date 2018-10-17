ee0678b8ac81c163cc487625d1edfbeefed7f4ea
duyanhnn <duyanhnguyenngoc97@gmail.com>
2018-10-17 21:03:44 +0700

    msg.

--- ssh/citynow-ec2-?.sh
+++ ssh/citynow-ec2-?.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 chmod 400 books_production_key.pem
-ssh -i books_production_key.pem ubuntu@54.168.100.61
-#ssh ec2-user@54.168.100.61
-#ssh root@54.168.100.61
-#ssh fedora@54.168.100.61
-#ssh admin@54.168.100.61
+sudo ssh -i books_production_key.pem ubuntu@54.168.100.61
+#sudo ssh ubuntu@54.168.100.61
+#sudo ssh ec2-user@54.168.100.61
+#sudo ssh root@54.168.100.61
+#sudo ssh fedora@54.168.100.61
+#sudo ssh admin@54.168.100.61
+
+# check $HOME/.ssh/authorized_keys
