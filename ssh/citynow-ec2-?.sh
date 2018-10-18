36cdfbb35e0378353bc7bd1f2be9abfdd732c783
duyanhnn <duyanhnguyenngoc97@gmail.com>
2018-10-18 12:51:53 +0700

    msg.

--- ssh/citynow-ec2-?.sh
+++ ssh/citynow-ec2-?.sh
@@ -9,3 +9,5 @@ sudo ssh -i books_production_key.pem ubuntu@54.168.100.61
 #sudo ssh admin@54.168.100.61
 
 # check $HOME/.ssh/authorized_keys
+# https://askubuntu.com/questions/466549/bash-home-user-ssh-authorized-keys-no-such-file-or-directory
+# https://stackoverflow.com/questions/18551556/permission-denied-publickey-when-ssh-access-to-amazon-ec2-instance
