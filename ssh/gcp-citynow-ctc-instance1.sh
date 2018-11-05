1aff83f54e97cd5c01ad3ad56dc162c0e6df485a
duyanhnn <duyanhnguyenngoc97@gmail.com>
2018-11-05 17:13:02 +0700

    .

--- ssh/gcp-citynow-ctc-instance1.sh
+++ ssh/gcp-citynow-ctc-instance1.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 gcloud config set account "onlyu.citynow.gcp1@gmail.com"
-gcloud config set project fluted-quasar-218508
-gcloud compute --project "fluted-quasar-218508" ssh --zone "asia-southeast1-b" "ctc-instance-1"
+gcloud config set project ctcDeployment
+gcloud compute --project "ctcdeployment" ssh --zone "australia-southeast1-b" "instance-1"
